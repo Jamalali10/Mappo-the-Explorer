d81e9e1a5de90b7e19c984ae0e7d2cf2ed27629c
MrNuTTr <43860787+MrNuTTr@users.noreply.github.com>
2023-03-02 19:53:50 -0500

    Trying to get reading from sensors

--- SRC/main/main.ino
+++ SRC/main/main.ino
@@ -1,20 +1,27 @@
 #include <Adafruit_MotorShield.h>
-#include"RobotController.h"
+#include "RobotController.h"
 
-AFMSController AFMS = AFMSController(255, Adafruit_MotorShield());
+AFMSController AFMS = AFMSController(100, Adafruit_MotorShield());
 
 void setup() {
+  Serial.begin(9600);
   AFMS.begin();
 }
 
 void loop() {
   int val = analogRead(A0);
+  //int ir_cm = exp(8.5841-log(val));
+  // https://www.makerguides.com/sharp-gp2y0a21yk0f-ir-distance-sensor-arduino-tutorial/
+  //int ir_cm = 29.988 * pow(val, -1.173);
   int ir_cm = exp(8.5841-log(val));
+  Serial.print("IR Value: ");
+  Serial.println(ir_cm); 
 
-  AFMS.forward();
-  delay(2000);
+  if(ir_cm < 15) {
+    delay(500);
+    AFMS.stop();
+  }
+
   AFMS.stop();
-  delay(2000);
-  AFMS.backward();
-  delay(2000);
+  delay(10);
 }
