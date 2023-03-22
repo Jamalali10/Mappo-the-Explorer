d23c56e31161981286d06bea46ad8c6d6fe3efa1
Jamalali10 <Jamalali000005@gmail.com>
2023-03-22 16:39:07 -0400

    Merge branch 'main' of https://github.com/Jamalali10/vacuum-less-robot

--- SRC/main/main.ino
+++ SRC/main/main.ino
@@ -9,27 +9,49 @@ RobotController robot = RobotController(defaultRobotSpeed, Adafruit_MotorShield(
 // WindowSize for noise filtering. 0 if not utilizing
 // I made this 0 because the window class is broke rn gotta debug
 int windowSize = 0;
-int irPins[4] = {A0, A1, A2, A3};
-IR sensor[4];
+IR sensor[4] = {
+  IR(A0, windowSize),
+  IR(A1, windowSize),
+  IR(A2, windowSize),
+  IR(A3, windowSize)
+};
 
 void setup() {
   Serial.begin(9600);
   robot.begin();
-  for(int i = 0; i < 4; i++) {
-    sensor[i] = IR(irPins[i], windowSize);
-  }
 }
 
 void loop() {
-  robot.stop();
+  robot.forward();
+  int ir_cm = 0;
 
-  for(int i = 0; i < 4; i++) {
-    Serial.print("IR Values: ");
-    Serial.print(sensor[i].readValue());
-    Serial.print(" ");
-  }
+  Serial.print("IR Values: ");
 
+  ir_cm = sensor[3].readValue();
+
+  Serial.print(ir_cm);
+  
+//  for(int i = 0; i < 4; i++) {
+//    ir_cm = sensor[i].readValue();
+//    average += ir_cm;
+//    Serial.print(ir_cm);
+//    Serial.print(" ");
+//  }
   Serial.println(" ");
+<<<<<<< HEAD
   delay(200);
 
+=======
+
+<<<<<<< HEAD
+  if (ir_cm < 15) {
+    robot.backward();
+    delay(1500);
+  }
+  else {
+    robot.forward();
+  }
+
+  delay(100);
+>>>>>>> b5179545b721773fb9f4952673402f045d43337f
 }
